affccee6bb8d5543d820642ba4683c61379ad01f
posmansitinjak06-creator <posmansitinjak06@gmail.com>
2025-11-18 21:15:51 +0700

    posman
    done

--- TLAST.java
+++ TLAST.java
@@ -1,2 +1,17 @@
-//Nama - NIM
+//Posman H.C Sitinjak
+
+import java.util.*;
+import java.lang.Math;
+
+public class JavaApplication {
+    public static void main(String[] args) {
+        String[] deskripsitugas = new String[10], kodematkul = new String[10], dosen = new String[10], deadline = new String[10], nimnama = new String[10], status = new String[10], matkul = new String[10], teks = new String[10];
+        double[] tingkatkesulitan = new double[10], prioritas = new double[10];
+        int[] harihinggadeadline = new int[10];
+        String task;
+        int i, n;
+
+        n = 0;
+    }
+}
 
